a37d6d4793d63b095fc7c82f7ef821ee6cb30005
Kirthick Sakthi V <158608170+KirthickSakthiV@users.noreply.github.com>
2024-02-16 04:56:41 -0100

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,33 @@
+Circle
+
+
+import java.util.*;
+
+class Circle{
+  double rad;
+  Circle(double rad){
+    this.rad = rad;
+  }
+  void printResult(){
+    System.out.printf("Radius of the circle is %.0f\n",rad);
+    System.out.println("The area of the circle is "+(Math.PI*rad*rad));
+    System.out.println("The circumference of the circle is "+(2*Math.PI*rad));
+  }
+}
+
+class CircleOops{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    Circle[] c;
+    c = new Circle[n];
+    double r;
+    for(int i = 0; i < n; i++){
+      r = in.nextDouble();
+      c[i] = new Circle(r);
+    }
+    for(int i = 0; i < n; i++){
+      c[i].printResult();
+    }
+  }
+}
